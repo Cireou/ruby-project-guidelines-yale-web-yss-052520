2a23a39685f72a70c5a28be2c25058233a2310a1
Eric Ou <eric.ou@yale.edu>
2020-06-11 13:53:29 -0700

    finished profile and searcher

--- bin/reppage.rb
+++ bin/reppage.rb
@@ -2,15 +2,12 @@ require_relative '../config/environment'
 class RepPage 
     @@prompt = TTY::Prompt.new
     def self.load_search_engine()
-        rep = nil
-        while !rep 
-            rep_name = @@prompt.ask("Who would you like to look for (First Name + Last Name?)")
-            rep = Representative.find_by(name: rep_name)
-            if !rep 
-                request = @@prompt.yes?("Could not find that representative, would you like to look for another or try again?")
-                return if request == 'no'
-            end
-            load_rep_page(rep)
+        while true 
+            rep = load_searcher()
+            return if !rep
+
+            load_profile(rep)
+            return if !load_extra_info()
         end
     end
 
@@ -21,7 +18,45 @@ class RepPage
     end
 
     private
-    def self.load_rep_page(rep)
 
+    def self.load_searcher()
+        rep = nil
+        while !rep 
+            rep_name = @@prompt.ask("Who would you like to look for (First Name + Last Name?)")
+            rep = Representative.find_by(name: rep_name)
+            # binding.pry
+            if !rep 
+                system("clear")
+                request = @@prompt.yes?("Could not find that representative, would you like to look for another or try again?")
+                # binding.pry
+                return if !request
+            end
+            system("clear")
+        end
+        rep
+    end
+
+    def self.load_profile(rep)
+        puts "Name: #{rep[:name]}"
+        puts "Age: #{rep[:age]}"
+        puts "Years of Experience: #{rep[:experience]}"
+        puts "Party: #{getparty(rep[:party])}"
+        puts "State Abbreviation: #{rep[:state]}"
+        puts "Next Election: #{rep[:next_election]}"
+    end
+
+    def self.load_extra_info()
+        
+    end
+
+    def self.getparty(party_string)
+        case party_string
+        when "R"
+            return "Republican"
+        when "D"
+            return "Democrat"
+        when "I"
+            return "Independent"
+        end
     end
 end
